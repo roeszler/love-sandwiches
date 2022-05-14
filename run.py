0dd5299efd25557ca90b1a155dc3616c6609c0df
Stuart Roeszler <stuart@roeszler.com>
2022-05-14 08:09:16 +0000

    FEAT: add vlaidation check for exactly 6 vlaues to be input for new data

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-# sales = SHEET.worksheet('sales')
+# sales = SHEET.worksheet('sales') # just to check the sales sheet connection is working
 # data = sales.get_all_values()
 # print(data)
 
@@ -22,6 +22,26 @@ def get_sales_data():
     print('Example: 10,20,30,40,50,60\n')
 
     data_str = input('Enther your data here: ')
-    print(f'The data provided is {data_str}')
+    # print(f'The data provided is {data_str}') # just to check the string inp ut is being recieved correctly
+
+    sales_data = data_str.split(',')
+    # print(f'The data you provided converted into a list is:\n{sales_data}')
+    validate_data(sales_data) # calls the function below and puts the .split() data into it
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int or
+    if there are not exactly 6 values.
+    """
+    # print(f'The data you provided converted into a list is:\n{values}')
+    try:
+        if len(values) !=6: #if the length of the values input 'does not equal' 6
+            raise ValueError(
+                f'`Exactly 6 values required, you provided {len(values)}'
+            )
+    # ValueError class contains the details of the error (as defined above). By using the 'as' keyword we are assigning the ValueError object to the variable 'e' meaning 'error':
+    except ValueError as e:
+        print(f'Invalid data : {e}, please try again.\n')
 
 get_sales_data()
